442fca1c9480a67ee091cd5ae153cb16246aa193
Simeon Platonov <simeon.platonov@gmail.com>
2022-04-21 10:25:00 +0300

    Initializing incidents.php

---
+++ incidents/incidents.php
@@ -0,0 +1,15 @@
+<?php
+
+defined ( 'ABSPATH' ) || die ( 'No script kiddies please!' );
+
+require_once( __DIR__ . '/incidents-type.php');
+require_once( __DIR__ . '/incidents-rest.php');
+require_once( __DIR__ . '/incidents-settings.php');
+require_once( __DIR__ . '/incidents-taxonomy.php');
+require_once( __DIR__ . '/acf.php');
+
+add_action('admin_enqueue_scripts', function () {
+  wp_enqueue_script('incident-acf-js', plugin_dir_url(__FILE__) . 'acf.js', array(), INCIDENTS_PLUGIN_VERSION, true);
+});
+
+?>
